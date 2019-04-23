1254cd8a03470021741b495c960fa8bcabe5ea9c
Sitang.Ruan <Sitang.Ruan@hikvision.com>
2019-04-23 18:19:24 -0400

    Add first test method with TestHelper for dependency injection

---
+++ nini.core.test/TestHelper.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace nini.core.test
+{
+    public static class TestHelper
+    {
+        public static ContainerBuilder CreateBuilderWithDefaultModules()
+        {
+            var myBuilder = new ContainerBuilder();
+            myBuilder.RegisterModule<niniCoreModule>();
+
+            return myBuilder;
+        }
+    }
+}
--- nini.core.test/V10/ValuesManagerTest.cs
+++ nini.core.test/V10/ValuesManagerTest.cs
@@ -1,4 +1,6 @@
 using System;
+using Autofac;
+using nini.core.V10;
 using Xunit;
 
 namespace nini.core.test
@@ -8,7 +10,17 @@ namespace nini.core.test
         [Fact]
         public void GetValuesTest()
         {
-            //To be implemented
+            ContainerBuilder builder = TestHelper.CreateBuilderWithDefaultModules();
+            IContainer container = builder.Build();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var manager = scope.Resolve<V10.IValuesManager>();
+
+                string[] values = manager.GetValues();
+
+                Assert.Equal("Value 1 from mgr", values[0]);
+                Assert.Equal("Value 2 from mgr", values[1]);
+            }
         }
     }
 }
