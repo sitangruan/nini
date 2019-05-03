c31df5c7517a2f9048a6f39163b4e099c6db636b
Sitang.Ruan <Sitang.Ruan@hikvision.com>
2019-05-03 18:08:10 -0400

    Add BaseController

---
+++ nini/common/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog;
+
+namespace nini.Common
+{
+    public abstract class BaseController<T>: Controller where T : class
+    {
+        protected readonly ILogger<T> Logger;
+
+        protected BaseController(ILogger<T> logger)
+        {
+            Logger = logger;
+        }
+    }
+}
